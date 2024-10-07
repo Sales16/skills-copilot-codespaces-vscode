d59e5ac010008d88e6ba2ecfb5ade830236fb5f2
Eduardo Sales <145076606+Sales16@users.noreply.github.com>
2024-10-07 14:18:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = new Member();
+    member.skills = [
+        "Javascript",
+        "Node.js",
+        "MongoDB",
+        "Express.js"
+    ];
+    return member.skills;
+}
